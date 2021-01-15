599225a883b022583c5fc5d400ecae94bf45513c
Rajesh <chilaganirajesh95@gmail.com>
2021-01-14 21:48:26 -0700

    Clean up

--- poll/views.py
+++ poll/views.py
@@ -1,16 +1,54 @@
-from django.shortcuts import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('poll/index.html')
     context = {
-        'latest_question_list': latest_question_list,
+        'latest_question_list': latest_question_list
     }
-    return HttpResponse(template.render(context,request))
+    return render(request,'poll/index.html',context)
 def test(request):
     return HttpResponse("This is a test")
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,'poll/detail.html',{'question':question})
+def results(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'poll/results.html', {'question': question})
+
+def vote(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,'poll/detail.html',{'question':question,
+        'error_message':"Select an option",})
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('poll:results', args=(question.id,)))
+
+class IndexView(generic.ListView):
+    template_name = 'poll/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'poll/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'poll/results.html'
